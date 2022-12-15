28c053daafde0ec61f370b75c27d33b8b4ab2f87
Do Van Thanh <thanhdv7454@co-well.vn>
2022-12-15 17:28:02 +0700

    up

--- ts/settings.py
+++ ts/settings.py
@@ -43,21 +43,6 @@ DATABASES = {
     }
 }
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
 STATIC_URL = 'static/'
 
 WSGI_APPLICATION = 'wsgi.application'

8dd86c595069e8b8b55b8f3d05f6277e794b982a
Do Van Thanh <thanhdv7454@co-well.vn>
2022-12-15 17:49:21 +0700

    up

--- ts/manage.py
+++ ts/manage.py
@@ -1,11 +1,5 @@
-import os
-import sys
+import os, sys
 
-
-def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
-    from django.core.management import execute_from_command_line
-    execute_from_command_line(sys.argv)
-
-if __name__ == '__main__':
-    main()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+from django.core.management import execute_from_command_line
+execute_from_command_line(sys.argv)
